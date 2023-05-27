5a7c7a70f960b39058f85185c93622f5a3981a49
zerokivin <kivin159@yandex.ru>
2023-05-27 13:13:13 +0300

    add order history screen 4

--- lib/feature/order_history/ui/order_history_page.dart
+++ lib/feature/order_history/ui/order_history_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:farm_app/util/money_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -53,51 +54,82 @@ class OrderHistoryPage extends StatelessWidget {
                     final order = orders[index];
 
                     return Card(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text('№ ${order.id}'),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Text(order.full_price?.formatMoney() ?? ''),
-                            ],
-                          ),
-                          ListView.separated(
-                            physics: const BouncingScrollPhysics(),
-                            itemBuilder: (context, index) {
-                              final item = order.items[index];
-
-                              return SizedBox(
-                                width: 60,
-                                height: 80,
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Image.network(
-                                      item.picture,
-                                      height: 60,
-                                      width: 60,
-                                      fit: BoxFit.contain,
-                                    ),
-                                    Text(
-                                      'Количество: ${item.count}',
-                                      style: textTheme.bodySmall?.copyWith(
-                                        color: colorTheme.onBackground,
-                                      ),
-                                    ),
-                                  ],
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Заказ № ${order.id}',
+                                  style: textTheme.bodyMedium?.copyWith(
+                                    color: colorTheme.onBackground,
+                                  ),
                                 ),
-                              );
-                            },
-                            separatorBuilder: (_, __) => const SizedBox(
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Text(
+                                  'Цена: ${order.full_price?.formatMoney() ?? ''}',
+                                  style: textTheme.bodyMedium?.copyWith(
+                                    color: colorTheme.onBackground,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
                               height: 10,
                             ),
-                            itemCount: order.items.length,
-                          ),
-                        ],
+                            SizedBox(
+                              height: 120,
+                              child: ListView.separated(
+                                scrollDirection: Axis.horizontal,
+                                physics: const BouncingScrollPhysics(),
+                                itemBuilder: (context, index) {
+                                  final item = order.items[index];
+
+                                  return SizedBox(
+                                    width: 100,
+                                    height: 120,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: const BorderRadius.all(
+                                            Radius.circular(
+                                              20,
+                                            ),
+                                          ),
+                                          child: CachedNetworkImage(
+                                            height: 100,
+                                            width: 100,
+                                            fit: BoxFit.contain,
+                                            imageUrl: item.picture,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Количество: ${item.count}',
+                                          style: textTheme.bodySmall?.copyWith(
+                                            color: colorTheme.onBackground,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                                separatorBuilder: (_, __) => const SizedBox(
+                                  height: 10,
+                                ),
+                                itemCount: order.items.length,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
